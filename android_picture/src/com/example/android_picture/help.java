a73204918d63b8d831beffe39288c0eeb58f63b3
xlm7 <1182789409@qq.com>
2015-11-24 15:29:20 +0800

    迷你动画

---
+++ android_picture/src/com/example/android_picture/help.java
@@ -0,0 +1,17 @@
+package com.example.android_picture;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View.OnCreateContextMenuListener;
+
+public class help extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.help);
+		
+	}
+	}
+
